413465aef08c08256776db0978d23b378a283a6f
Yefeng Wu <yefengwu@yefengs-mbp.lan>
2021-06-22 11:16:12 -0400

    add callback route

--- application.py
+++ application.py
@@ -1,24 +1,28 @@
 from flask import Flask
+from flask import request
+from flask.wrappers import Response
+import requests
+
 
 application = Flask(__name__)
+if __name__ == "__main__":
+    application.run()
+
 
 @application.route('/')
 def hello_world():
     return 'Hello World!!!'
 
+
 @application.route('/healthy')
 def healthy():
     return 'healthy'
 
-"""
-@app.route('/callback')
-def hello() -> Response:
+
+@application.route('/callback')
+def callback() -> Response:
     token = request.args.get('access_token')
-    import requests
+    
     header_data = {'Authorize': token}
     response = requests.get('https://gyhot27101.execute-api.us-east-1.amazonaws.com/test/hello', headers=header_data).content
     return response
-"""
-
-if __name__ == "__main__":
-    application.run()
