bb2b744c6db05f987d8b48e904f885c2fdef9570
Yefeng Wu <yefengwu@Yefengs-MacBook-Pro.local>
2021-06-23 10:17:31 -0400

    modify

--- application.py
+++ application.py
@@ -19,25 +19,16 @@ def healthy():
 @application.route('/test')
 def test():
     token = request.args.get('access_token')
-    return token
+    
+    header_data = {'Authorize': token}
+    response = requests.get('https://gyhot27101.execute-api.us-east-1.amazonaws.com/test/hello', headers=header_data).content
+    return response
 
 
 @application.route('/callback')
 def callback() -> Response:
     return render_template('home.html')
 
-    '''
-    token = request.args.get('access_token')
-    
-    header_data = {'Authorize': token}
-    response = requests.get('https://gyhot27101.execute-api.us-east-1.amazonaws.com/test/hello', headers=header_data).content
-    return response
-    '''
 
 if __name__ == "__main__":
-    '''
-    from urllib.parse import urlparse
-    url = 'https://www.google.com/callback#id=3a'
-    print(urlparse(url).fragment)
-    '''
     application.run()
